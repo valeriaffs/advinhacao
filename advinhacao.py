1686352260cd6e9c468f9bc7f7fb4100b3b958f1
Valéria Ferreira <valeriaffs@hotmail.com>
2020-09-30 16:48:15 -0300

    usando for e criação de níveis

--- advinhacao.py
+++ advinhacao.py
@@ -5,8 +5,22 @@ print("******************************************")
 numero = 42
 total_tentativas = 3
 rodada = 1
+nivel = 0
 
-while(rodada <= total_tentativas):
+print("Nivel 1, você tem 20 tentativas")
+print("Nivel 2, você tem 10 tentativas")
+print("Nivel 3, você tem 5 tentativas")
+
+nivel = int( input(" digite um nível "))
+
+if(nivel == 1):
+    total_tentativas = 20
+elif(nivel == 2):
+    total_tentativas = 10
+elif(nivel == 3):
+    total_tentativas = 5 
+
+for rodada in range(1, total_tentativas):
     print("Tentativa {} de {} ".format(rodada, total_tentativas))
     chute = int (input("Digite um número: "))
     acertou = chute == numero
